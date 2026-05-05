b471c92c1c8eea3667ed100e7036cdd28cc23382
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2026-05-05 11:10:50 +0200

    Feat: Ajout du modèle CoffretType

---
+++ gift.appli/src/models/CoffretType.php
@@ -0,0 +1,13 @@
+<?php
+namespace gift\appli\models;
+use Illuminate\Database\Eloquent\Model;
+use gift\appli\models\Prestation;
+
+class CoffretType extends Model {
+    protected $table = 'coffret_type';
+    protected $primaryKey = 'id';
+
+    
+   
+}
+?>
