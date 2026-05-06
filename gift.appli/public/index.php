c041bf3cc63070d513d24d0e032263310c3e3e59
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2026-05-06 14:09:21 +0200

    Fix: réparation de la redirections des routes

--- gift.appli/public/index.php
+++ gift.appli/public/index.php
@@ -2,12 +2,16 @@
 declare(strict_types=1);
 
 use Slim\Factory\AppFactory;
+use gift\appli\utils\Eloquent;
 
 session_start();
 
 require_once __DIR__ . '/../src/vendor/autoload.php';
 
+Eloquent::init(__DIR__ . '/../src/conf/db.ini');
+
 $app = AppFactory::create();
+$app->setBasePath('/giftbox');
 
 $routes = require_once __DIR__ . '/../src/conf/routes.php';
 $routes($app);
