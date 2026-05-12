8252bfe599cd5cceac8cb879bca25ec8b4823acb
AIME--CABOCEL Léandre <leandreaimecabocel@gmail.com>
2026-05-12 10:17:07 +0200

    Ajout du controler

---
+++ gift.appli/src/controlers/GetCoffretByIdAction.php
@@ -0,0 +1,27 @@
+<?php
+namespace gift\appli\controlers;
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use gift\appli\models\Categorie;
+use Slim\Views\Twig;
+
+class GetCoffretByIdAction {
+
+    public function __invoke(Request $rq, Response $rs, array $args): Response {
+        $id = $args['id'];
+
+        try {
+            $coffretType = CoffretType::findOrFail($id);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            throw new \Slim\Exception\HttpNotFoundException($rq, 'Coffret introuvable');
+        }
+
+        $view = Twig::fromRequest($rq);
+        return $view->render($rs, 'coffretByIdView.twig', [
+            'coffretType' => $coffretType->toArray(),
+            'prestations'  => $coffretType->prestations->toArray(),
+        ]);
+
+    }
+}
