8da7401d75ac8df0db81e85a6e614850cddddace
Boudouah Ilias <ilias.boudouah8@etu.univ-lorraine.com>
2026-05-12 09:47:15 +0200

    [FIX] Routes avec Xampp

--- gift.appli/public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-require_once __DIR__ . '/../src/vendor/autoload.php';
-/* application bootstrap */
-$app = require_once __DIR__ . '/../src/conf/bootstrap.php';
-$app->run();
-
-
--- gift.appli/src/conf/bootstrap.php
+++ gift.appli/src/conf/bootstrap.php
@@ -1,5 +1,4 @@
 <?php
-
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
@@ -7,23 +6,29 @@ use gift\appli\utils\Eloquent;
 
 session_start();
 
+// src/conf/db.ini
 Eloquent::init(__DIR__ . '/db.ini');
 
 $app = AppFactory::create();
-$app->setBasePath('/giftbox');
 
+// Détection automatique de l'environnement
+$basePath = $_SERVER['SERVER_NAME'] === 'localhost'
+    ? '/giftbox/gift.appli'
+    : '/giftbox';
+$app->setBasePath($basePath);
+
+// src/conf/../../src/views/templates = src/views/templates
 $twig = Twig::create(__DIR__ . '/../views/templates', ['cache' => false]);
 
 $twig->getEnvironment()->addGlobal('nav_menu', [
-    ['url' => '/giftbox/home', 'label' => 'Accueil'],
-    ['url' => '/giftbox/categories', 'label' => 'Lister les catégories']
-
+    ['url' => $basePath . '/', 'label' => 'Accueil'],
+    ['url' => $basePath . '/categories', 'label' => 'Lister les catégories']
 ]);
-$twig->getEnvironment()->addGlobal('css_dir', '/giftbox/css');
-$twig->getEnvironment()->addGlobal('img_dir', '/giftbox/img');
-
+$twig->getEnvironment()->addGlobal('css_dir', $basePath . '/public/css');
+$twig->getEnvironment()->addGlobal('img_dir', $basePath . '/public/img');
 $app->add(TwigMiddleware::create($app, $twig));
 
+// src/conf/routes.php
 $routes = require_once __DIR__ . '/routes.php';
 $routes($app);
 
