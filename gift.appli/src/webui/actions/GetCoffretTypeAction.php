0d4e6c19647fa52c4a54100bf53fb71293029d36
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2026-05-19 09:23:24 +0200

    file: changement d'emplacement du fichier exception

--- gift.appli/src/webui/actions/GetCoffretTypeAction.php
+++ gift.appli/src/webui/actions/GetCoffretTypeAction.php
@@ -4,13 +4,20 @@ namespace gift\appli\webui\actions;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 use gift\appli\application_core\application\useCases\CatalogueService;
+use gift\appli\application_core\application\exceptions\EntityNotFoundException;
 use Slim\Views\Twig;
 
 
 class getCoffretTypeAction{
     public function __invoke(Request $rq, Response $rs, array $args): Response {
-        $service = new CatalogueService();
-        $coffrets = $service->getThemesCoffrets();
+        try {
+            $service = new CatalogueService();
+            $coffrets = $service->getThemesCoffrets();
+        } catch (EntityNotFoundException $e) {
+            throw new \Slim\Exception\HttpNotFoundException($rq, $e->getMessage());
+        } catch (\Exception $e) {
+            throw new \Slim\Exception\HttpInternalServerErrorException($rq, $e->getMessage());
+        }
 
         $view = Twig::fromRequest($rq);
         return $view->render($rs, 'coffretTypeView.twig', ['coffretType' => $coffrets]);
--- gift.appli/src/webui/actions/GetPrestationsListAction.php
+++ gift.appli/src/webui/actions/GetPrestationsListAction.php
@@ -4,12 +4,19 @@ namespace gift\appli\webui\actions;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 use gift\appli\application_core\application\useCases\CatalogueService;
+use gift\appli\application_core\application\exceptions\EntityNotFoundException;
 use Slim\Views\Twig;
 
 class GetPrestationsListAction {
     public function __invoke(Request $rq, Response $rs, array $args): Response {
-        $catalogueService = new CatalogueService();
-        $prestations = $catalogueService->getPrestations();
+        try {
+            $catalogueService = new CatalogueService();
+            $prestations = $catalogueService->getPrestations();
+        } catch (EntityNotFoundException $e) {
+            throw new \Slim\Exception\HttpNotFoundException($rq, $e->getMessage());
+        } catch (\Exception $e) {
+            throw new \Slim\Exception\HttpInternalServerErrorException($rq, $e->getMessage());
+        }
 
         $view = Twig::fromRequest($rq);
         return $view->render($rs, 'prestationsListView.twig', ['prestations' => $prestations]);
