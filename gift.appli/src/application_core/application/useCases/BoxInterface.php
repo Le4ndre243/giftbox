01e96f7661ab05e76e7753550dcdc38ed63fd972
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2026-05-27 14:39:17 +0200

    feat: ajout de addPrestation dans BoxInterface

--- gift.appli/src/application_core/application/useCases/BoxInterface.php
+++ gift.appli/src/application_core/application/useCases/BoxInterface.php
@@ -7,6 +7,8 @@ interface BoxInterface {
 
     public function createBox(string $libelle, string $description, bool $kdo, string $message_kdo): Box;
 
+    public function addPrestation(string $box_id, string $presta_id): void;
+
     public function generateToken(string $box_id): string;
 
     public function getBoxByToken(string $token): array;
